772d1879121f599d1a261309081a37c8f7104439
SamanehAlimohammadi <a.smneh792gmail.com>
2024-12-31 17:02:44 +0330

    feat: add User entity

---
+++ src/Core/Domain/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Domain.Entities;
+
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; } 
+    
+    [Required] 
+    [MaxLength(64)]
+    public required string Username { get; set; }
+    
+    [MaxLength(128)]
+    public string? FullName { get; set; }
+    
+    [Required] 
+    [MaxLength(320)]
+    public required string Email { get; set; }
+    
+    [Required] 
+    [MaxLength(128)]
+    public required string Password { get; set; }
+    
+    [Required]
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    
+    public DateTime? LastModifiedDate { get; set; }
+}
