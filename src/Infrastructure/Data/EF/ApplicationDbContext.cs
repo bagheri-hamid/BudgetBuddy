8ea94e49d722ae0f224d6c6aa89e4d36870f6ace
SamanehAlimohammadi <a.smneh792gmail.com>
2024-12-31 17:14:05 +0330

    feat: add Application DB Context

---
+++ src/Infrastructure/Data/EF/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Core.Application.Interfaces;
+using Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.EF;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options), IApplicationDbContext
+{
+    public DbSet<User> Users { get; set; }
+}
