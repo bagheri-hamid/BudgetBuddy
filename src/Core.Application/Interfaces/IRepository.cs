1d26cf7e5aa1f8b679b99bf729caae904a6a5288
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-01-26 12:47:59 +0330

    refactor(repository): Add Async suffix to asynchronous methods in IRepository
    - Rename all async methods to follow .NET Task-based async pattern conventions
    - Maintain consistency with common C# async/await practices
    - Improve code clarity for async operations

--- src/Core.Application/Interfaces/IRepository.cs
+++ src/Core.Application/Interfaces/IRepository.cs
@@ -4,14 +4,14 @@ namespace Core.Application.Interfaces;
 
 public interface IRepository<T> where T : class
 {
-    Task<IEnumerable<T>> GetAll();
-    Task<T?> GetById();
-    Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate);
-    Task<T?> FindOne(Expression<Func<T, bool>> predicate);
-    Task Add(T entity);
-    Task AddRange(IEnumerable<T> entities);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync();
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<T?> FindOneAsync(Expression<Func<T, bool>> predicate);
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
     void Update(T entity);
     void Remove(T entity);
     void RemoveRange(IEnumerable<T> entities);
-    Task SaveChanges();
+    Task SaveChangesAsync();
 }
