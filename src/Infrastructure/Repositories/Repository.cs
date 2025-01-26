2aeea80ddebf70ce5d16c0aea5b8f69eb6592cf7
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-01-26 13:51:16 +0330

    feat(repository): Implement async repository pattern with EF Core integration
    - Add concrete Repository<T> class implementing updated IRepository<T> interface
    - Utilize EF Core async methods (ToListAsync, FindAsync, AddAsync, etc)
    - Support both Guid and object ID types with GetByIdAsync overloads
    - Include comprehensive XML documentation for all methods
    - Implement proper async/await pattern throughout data access layer

---
+++ src/Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,119 @@
+﻿using System.Linq.Expressions;
+using Core.Application.Interfaces;
+using Infrastructure.Data.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class Repository<T>(ApplicationDbContext context) : IRepository<T> where T : class
+{
+    protected readonly DbSet<T> DbSet = context.Set<T>();
+    
+    /// <summary>
+    /// Retrieves all entities of type T from the database.
+    /// </summary>
+    /// <returns>A list of all entities of type T.</returns>
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await DbSet.ToListAsync();
+    }
+
+    /// <summary>
+    /// Retrieves an entity by its ID.
+    /// Throws an exception if the entity is not found.
+    /// </summary>
+    /// <param name="id">The ID of the entity to retrieve.</param>
+    /// <returns>The entity of type T with the specified ID.</returns>
+    public async Task<T?> GetByIdAsync(object id)
+    {
+        var entity = await DbSet.FindAsync(id);
+
+        return entity;
+    }
+
+    /// <summary>
+    /// Retrieves an entity by its ID.
+    /// Throws an exception if the entity is not found.
+    /// </summary>
+    /// <param name="id">The ID of the entity to retrieve.</param>
+    /// <returns>The entity of type T with the specified ID.</returns>
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        var entity = await DbSet.FindAsync(id);
+
+        return entity;
+    }
+    
+    /// <summary>
+    /// Finds entities that match the given predicate.
+    /// </summary>
+    /// <param name="predicate">The condition to filter entities.</param>
+    /// <returns>A list of entities that match the predicate.</returns>
+    public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await DbSet.Where(predicate).ToListAsync();
+    }
+    
+    /// <summary>
+    /// Finds entities that match the given predicate.
+    /// </summary>
+    /// <param name="predicate">The condition to filter entities.</param>
+    /// <returns>A list of entities that match the predicate.</returns>
+    public async Task<T?> FindOneAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await DbSet.Where(predicate).FirstOrDefaultAsync();
+    }
+    
+    /// <summary>
+    /// Adds a new entity of type T to the database.
+    /// </summary>
+    /// <param name="entity">The entity to add.</param>
+    public async Task AddAsync(T entity)
+    {
+        await DbSet.AddAsync(entity);
+    }
+    
+    /// <summary>
+    /// Adds a collection of new entities of type T to the database.
+    /// </summary>
+    /// <param name="entities">The entities to add.</param>
+    public async Task AddRangeAsync(IEnumerable<T> entities)
+    {
+        await DbSet.AddRangeAsync(entities);
+    }
+
+    /// <summary>
+    /// Updates an existing entity of type T in the database.
+    /// </summary>
+    /// <param name="entity">The entity to update.</param>
+    public void Update(T entity)
+    {
+        DbSet.Update(entity);
+    }
+
+    /// <summary>
+    /// Removes an entity of type T from the database.
+    /// </summary>
+    /// <param name="entity">The entity to remove.</param>
+    public void Remove(T entity)
+    {
+        DbSet.Remove(entity);
+    }
+    
+    /// <summary>
+    /// Removes a collection of entities of type T from the database.
+    /// </summary>
+    /// <param name="entities">The collection of entities to remove.</param>
+    public void RemoveRange(IEnumerable<T> entities)
+    {
+        DbSet.RemoveRange(entities);
+    }
+    
+    /// <summary>
+    /// Saves all changes made to the database context.
+    /// </summary>
+    public async Task SaveChangesAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
