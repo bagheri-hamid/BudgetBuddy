63971efceee84a1dcc63f48f35c71b05368abd15
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-02-01 20:41:44 +0330

    feat: add generic response class and interface
    - Implement IResponse<T> interface with StatusCode, Message, IsSuccess, and Data properties
    - Add Response<T> class implementing IResponse<T> with constructor initialization

---
+++ src/WebApi/Interfaces/IResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Interfaces;
+
+public interface IResponse<T>
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public bool IsSuccess { get; set; }
+    public T Data { get; set; }
+}
