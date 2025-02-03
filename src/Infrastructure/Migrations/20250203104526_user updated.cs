dbf5c6f50c28103ddeb10f92a2a15643bb04b95f
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-02-03 14:39:08 +0330

    chore: add migrations for BaseEntity integration
    - Generate migrations to reflect BaseEntity changes in the database
    - Update User table schema to align with BaseEntity fields

---
+++ src/Infrastructure/Migrations/20250203104526_user updated.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class userupdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastModifiedDate",
+                table: "Users",
+                newName: "UpdatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Users",
+                newName: "CreatedAt");
+
+            // Drop the primary key constraint
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            // Drop the old IDENTITY column
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Users");
+
+            // Add the new GUID column
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                nullable: false);
+
+            // Re-add the primary key constraint
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "Users",
+                newName: "LastModifiedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Users",
+                newName: "CreatedDate");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
