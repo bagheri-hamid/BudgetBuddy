ab6e322d9b65f4eb8f841f7f61a594fbf365518b
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-02-04 13:01:30 +0330

    feat(WebApi): add TokenHelper for JWT user ID extraction

---
+++ src/WebApi/Helpers/TokenHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using Core.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApi.Helpers;
+
+public class TokenHelper(IHttpContextAccessor httpContextAccessor) : ITokenHelper, IScopedDependency
+{
+    public async Task<Guid> GetUserIdAsync()
+    {
+        var userIdStr = GetClaim(ClaimTypes.NameIdentifier);
+        
+        if (!Guid.TryParse(userIdStr, out var userId))
+            throw new UnauthorizedAccessException();
+
+        return userId;
+    }
+
+    private ClaimsPrincipal User => httpContextAccessor.HttpContext?.User ?? throw new UnauthorizedAccessException();
+    private string GetClaim(string claimType) => User.FindFirst(claimType)?.Value ?? throw new UnauthorizedAccessException();
+}
