777f532073307845b0fe1efda255273000ea7e2c
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-02-04 15:10:05 +0330

    feat(Category): add repository for category

---
+++ src/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Core.Application.Interfaces;
+using Core.Domain.Entities;
+using Infrastructure.Data.EF;
+
+namespace Infrastructure.Repositories;
+
+public class CategoryRepository(ApplicationDbContext context) : Repository<Category>(context), ICategoryRepository, IScopedDependency
+{
+    
+}
