6b2b1a502d71a751f5bbe76fb5c984c79df7ea84
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-02-04 15:11:55 +0330

    feat(Category): implement Api for create category

---
+++ src/WebApi/Controllers/V1/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Core.Domain.Commands.Category;
+using Core.Domain.Enums;
+using Core.Domain.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Helpers;
+using WebApi.ViewModels;
+
+namespace WebApi.Controllers.V1;
+
+public class CategoryController(IMediator mediator, IMapper mapper) : AuthorizedController
+{
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateCategoryCommand command)
+    {
+        var category = await mediator.Send(command);
+        var categoryViewModel = mapper.Map<CategoryViewModel>(category);
+
+        return ResponseHelper.CreateResponse(201, MessageEnum.CreatedSuccessfully.GetDescription(), true, categoryViewModel);
+    }
+}
