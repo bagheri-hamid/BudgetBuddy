0e2fad8b52be1fe34532266e58b2426295f94fb9
SamanehAlimohammadi <a.smneh792gmail.com>
2025-02-04 16:04:32 +0330

    feat(Account): implement service of create account

---
+++ src/Core.Application/Services/AccountService.cs
@@ -0,0 +1,30 @@
+﻿using Core.Application.Interfaces;
+using Core.Domain.Entities;
+using Core.Domain.Exceptions;
+
+namespace Core.Application.Services;
+
+public class AccountService(IAccountRepository accountRepository) : IAccountService, IScopedDependency
+{
+    public async Task<Account> CreateAccountAsync(string name, string type, long balance, Guid userId)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new EmptyFiledException(nameof(Account.Name));
+
+        if (string.IsNullOrWhiteSpace(type))
+            throw new EmptyFiledException(nameof(Account.Type));
+        
+        var account = new Account
+        {
+            Name = name,
+            Type = type,
+            Balance = balance,
+            UserId = userId,
+        };
+        
+        await accountRepository.AddAsync(account);
+        await accountRepository.SaveChangesAsync();
+        
+        return account;
+    }
+}
