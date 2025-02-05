f968837cdd2c709ea72778351695b8939cbfbb63
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-02-05 12:38:09 +0330

    chore(Category): remove service of category and implement all logic in handler

--- src/Core.Application/Handlers/Commands/Category/CreateCategoryHandler.cs
+++ src/Core.Application/Handlers/Commands/Category/CreateCategoryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Application.Interfaces;
 using Core.Domain.Commands.Category;
+using Core.Domain.Exceptions;
 using MediatR;
 
 namespace Core.Application.Handlers.Commands.Category;
@@ -14,10 +15,32 @@ namespace Core.Application.Handlers.Commands.Category;
 ///    with the provided name, parent category ID, and user ID.
 /// 3. Returning the newly created <see cref="Domain.Entities.Category"/> entity.
 /// </remarks>
-public class CreateCategoryHandler(ICategoryService categoryService, ITokenHelper tokenHelper) : IRequestHandler<CreateCategoryCommand, Domain.Entities.Category>
+public class CreateCategoryHandler(ICategoryRepository categoryRepository, ITokenHelper tokenHelper) : IRequestHandler<CreateCategoryCommand, Domain.Entities.Category>
 {
     public async Task<Domain.Entities.Category> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
     {
-        return await categoryService.CreateCategoryAsync(request.Name, request.ParentCategoryId, tokenHelper.GetUserId());
+        if (string.IsNullOrWhiteSpace(request.Name))
+            throw new EmptyFiledException(nameof(Domain.Entities.Category.Name));
+
+        if (request.ParentCategoryId != null)
+        {
+            var isExist = await categoryRepository.IsExistsAsync(c => c.Id == request.ParentCategoryId);
+
+            if (!isExist)
+                throw new ParentCategoryNotFoundException();
+        }
+
+        var category = new Domain.Entities.Category
+        {
+            Name = request.Name,
+            ParentCategoryId = request.ParentCategoryId,
+            UserId = tokenHelper.GetUserId(),
+        };
+        
+        await categoryRepository.AddAsync(category);
+        await categoryRepository.SaveChangesAsync();
+        
+        return category;
+        
     }
 }
