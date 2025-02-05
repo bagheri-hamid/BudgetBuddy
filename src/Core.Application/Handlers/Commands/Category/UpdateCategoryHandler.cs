95673d3db5390dfe94248d9b037a6cdcf807f295
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-02-05 12:40:35 +0330

    feat(Category): implement update category command and handler

---
+++ src/Core.Application/Handlers/Commands/Category/UpdateCategoryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Core.Application.Interfaces;
+using Core.Domain.Commands.Category;
+using Core.Domain.Exceptions;
+using MediatR;
+
+namespace Core.Application.Handlers.Commands.Category;
+
+public class UpdateCategoryHandler(ICategoryRepository categoryRepository, ITokenHelper tokenHelper) : IRequestHandler<UpdateCategoryCommand, Unit>
+{
+    public async Task<Unit> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = await categoryRepository.FindOneAsync(c => c.Id == request.Id && c.UserId == tokenHelper.GetUserId());
+
+        if (category == null)
+            throw new ObjectNotFoundException("Category");
+        
+        category.Name = request.Name;
+        
+        await categoryRepository.SaveChangesAsync();
+        
+        return Unit.Value;
+    }
+}
---
+++ src/Core.Domain/Commands/Category/UpdateCategoryCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Core.Domain.Commands.Category;
+
+public record UpdateCategoryCommand(Guid Id, string Name) : IRequest<Unit>;
