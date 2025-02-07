1b239d4b0a910ca2491d2b504ddafdc600607b02
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-02-07 21:13:02 +0330

    feat(Budget): implement creating budget

---
+++ src/Core.Domain/Commands/Budget/CreateBudgetCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Core.Domain.Commands.Budget;
+
+public record CreateBudgetCommand(long Amount, string Description, DateTime StartDate, DateTime EndDate, Guid? CategoryId) : IRequest<Entities.Budget>;
---
+++ src/WebApi/Controllers/V1/BudgetController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Core.Domain.Commands.Budget;
+using Core.Domain.Enums;
+using Core.Domain.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Helpers;
+using WebApi.ViewModels;
+
+namespace WebApi.Controllers.V1;
+
+public class BudgetController(IMediator mediator, IMapper mapper) : AuthorizedController
+{
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateBudgetCommand command)
+    {
+        var budget = await mediator.Send(command);
+        var budgetViewModel = mapper.Map<BudgetViewModel>(budget);
+        
+        return ResponseHelper.CreateResponse(201,  MessageEnum.CreatedSuccessfully.GetDescription(), true, budgetViewModel);
+    }
+}
