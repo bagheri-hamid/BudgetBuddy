77adfe6b7ba5c9aec61411797aeab3ee81f34274
SamanehAlimohammadi <a.smneh792gmail.com>
2025-02-08 11:23:20 +0330

    chore(Account): remove service of account and implement all logic in handler

--- src/Core.Application/Interfaces/IAccountService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Domain.Entities;
-
-namespace Core.Application.Interfaces;
-
-public interface IAccountService
-{
-    Task<Account> CreateAccountAsync(string name, string type, long balance, Guid userId);
-    Task<Account> UpdateAccountAsync(Guid accountId, string name, string type, long balance, Guid userId);
-}
