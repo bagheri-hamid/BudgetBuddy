a1da46fa372f28e6e08f197c6097ceb59629c197
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-03-02 16:07:57 +0330

    feat: implement Unit of Work pattern

---
+++ src/Core.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Core.Application.Interfaces;
+
+/// <summary>
+/// Provides a contract for unit-of-work implementations.
+/// </summary>
+public interface IUnitOfWork : IDisposable
+{
+    /// <summary>
+    /// Gets a repository for the specified entity.
+    /// </summary>
+    IRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+    
+    /// <summary>
+    /// Saves changes to the database.
+    /// </summary>
+    Task<int> CompleteAsync();
+    
+    /// <summary>
+    /// Begins a new database transaction.
+    /// </summary>
+    Task<IDbContextTransaction> BeginTransactionAsync();
+    
+    /// <summary>
+    /// Commits the current transaction.
+    /// </summary>
+    Task CommitAsync();
+    
+    /// <summary>
+    /// Rolls back the current transaction.
+    /// </summary>
+    Task RollbackTransactionAsync();
+}
